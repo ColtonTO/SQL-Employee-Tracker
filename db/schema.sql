8580162a66223f54714dc883caaee750d69feb24
ColtonTO <colton1105@gmail.com>
2023-02-03 14:46:38 -0800

    basic prompt structure and sql structure

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+USE company; 
+
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE roles (
+
+)
+
+CREATE TABLE departments (
+
+)
+
+CREATE TABLE employees (
+    
+)
